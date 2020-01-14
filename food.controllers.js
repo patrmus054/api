d789b728941f1f713a96c326476dd29fa0f5b1e8
Patryk <musiol.patryk97@gmail.com>
2020-01-14 23:49:55 +0100

    Changes made to app

--- food.controllers.js
+++
@@ -1,4 +0,0 @@
-const { crudControllers } = require("./crud");
-const Food = require("./food.model");
-
-module.exports = crudControllers(Food);
--- index.js
+++
@@ -1,6 +0,0 @@
-// import {
-//     start
-// } from "./server";
-const start = require("./server");
-
-start();
